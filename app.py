f14fd22797f087f4c19dd4c2f931a521a75d039c
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2026-05-03 11:02:58 +0200

    modification titre

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ st.markdown("""
     background: linear-gradient(90deg, #1f2937, #111827);
     color:white;
 ">
-    <h1 style="margin:0;">🍽️ Détection des sociétés HORECA</h1>
-    <p style="margin:0; font-size:16px; opacity:0.8;">
-        Analyse automatique des constitutions du Moniteur belge
+    <h1 style="margin:100;">🍽️ Détection des sociétés HORECA</h1>
+    <p style="margin:100; font-size:16px; opacity:0.8;">
+        Analyse sur base des annexes du Moniteur Belge.<br> Confirmation ultérieur via la BCE.
     </p>
 </div>
 """, unsafe_allow_html=True)
